7a92eed46b56d79801a8a8adf6d06f6181080cb5
Maxim Razinkov <vfx60@mail.ru>
2022-08-09 00:17:28 +0300

    commit

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,6 +2,21 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-
+        if (power < 0){
+            System.out.println("too much power");
+        }
+        else if (power == 0){
+            System.out.println(1);
+        }
+        else {
+            int counter = 1;
+            int temp = 1;
+            System.out.println(1);
+            while (counter <= power){
+                temp*=2;
+                System.out.println(temp);
+                counter++;
+            }
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,23 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int counter = 2;
+        boolean prime = false;
+        int i = 0;
+        while (counter <= printToInclusive){
+            prime = true;
+            i = 2;
+            while (i < counter){
+                if (counter % i == 0){
+                    prime = false;
+                    break;
+                }
+                i++;
+            }
+            if (prime){
+                System.out.println(counter);
+            }
+            counter++;
+        }
     }
 }
